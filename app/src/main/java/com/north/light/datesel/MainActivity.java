c758becef6653a0fe672295ddd4a10418a709349
2487177596@qq.com <13160675130king>
2020-06-22 22:03:31 +0800

    1完成数据显示

--- app/src/main/java/com/north/light/datesel/MainActivity.java
+++ app/src/main/java/com/north/light/datesel/MainActivity.java
@@ -3,6 +3,9 @@ package com.north.light.datesel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.north.light.libdatesel.DateMain;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateMain.getInstance().show(MainActivity.this,1);
+            }
+        });
     }
 }
--- libdatesel/src/main/java/com/north/light/libdatesel/ui/LibSelDateActivity.java
+++ libdatesel/src/main/java/com/north/light/libdatesel/ui/LibSelDateActivity.java
@@ -1,11 +1,13 @@
 package com.north.light.libdatesel.ui;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 import com.north.light.libdatesel.R;
 import com.north.light.libdatesel.widget.DivNumberPicker;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
@@ -45,12 +47,12 @@ public class LibSelDateActivity extends LibDateBaseActivity {
     private void initView() {
         mType = getIntent().getIntExtra(CODE_REQUEST, 1);
         //控件初始化
-        mYearPicker = findViewById(R.id.activity_lib_sel_address_content_year);
-        mMonthPicker = findViewById(R.id.activity_lib_sel_address_content_month);
-        mDayPicker = findViewById(R.id.activity_lib_sel_address_content_day);
-        mHourPicker = findViewById(R.id.activity_lib_sel_address_content_hour);
-        mMinutePicker = findViewById(R.id.activity_lib_sel_address_content_minute);
-        mSecondPicker = findViewById(R.id.activity_lib_sel_address_content_second);
+        mYearPicker = findViewById(R.id.activity_lib_sel_date_content_year);
+        mMonthPicker = findViewById(R.id.activity_lib_sel_date_content_month);
+        mDayPicker = findViewById(R.id.activity_lib_sel_date_content_day);
+        mHourPicker = findViewById(R.id.activity_lib_sel_date_content_hour);
+        mMinutePicker = findViewById(R.id.activity_lib_sel_date_content_minute);
+        mSecondPicker = findViewById(R.id.activity_lib_sel_date_content_second);
         //数据集合初始化
         //年
         Calendar startCal = Calendar.getInstance();
@@ -58,8 +60,7 @@ public class LibSelDateActivity extends LibDateBaseActivity {
         Calendar curCal = Calendar.getInstance();
         int startYear = startCal.get(Calendar.YEAR);
         int endYear = curCal.get(Calendar.YEAR);
-        int yearRange = endYear - startYear;
-        for (int i = yearRange; i >= startYear; i--) {
+        for (int i = endYear; i >= startYear; i--) {
             mYearList.add(i + "");
         }
         //月
@@ -83,10 +84,36 @@ public class LibSelDateActivity extends LibDateBaseActivity {
             mSecondList.add(i + "");
         }
         //设置数据----------------------------------------------------------
-        mYearPicker.setDisplayedValues();
-        mYearPicker.setMaxValue(provinceStrArray.length - 1); //设置最大值，最大值是datas[3]
+        mYearPicker.setDisplayedValues(mYearList.toArray(new String[mYearList.size()]));
+        mYearPicker.setMaxValue(mYearList.size() - 1); //设置最大值，最大值是datas[3]
         mYearPicker.setWrapSelectorWheel(true);
         mYearPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
+        mMonthPicker.setDisplayedValues(mMonthList.toArray(new String[mMonthList.size()]));
+        mMonthPicker.setMaxValue(mMonthList.size() - 1); //设置最大值
+        mMonthPicker.setWrapSelectorWheel(true);
+        mMonthPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
+        mDayPicker.setDisplayedValues(mDayList.toArray(new String[mDayList.size()]));
+        mDayPicker.setMaxValue(mDayList.size() - 1); //设置最大值
+        mDayPicker.setWrapSelectorWheel(true);
+        mDayPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
+        mHourPicker.setDisplayedValues(mHourList.toArray(new String[mHourList.size()]));
+        mHourPicker.setMaxValue(mHourList.size() - 1); //设置最大值
+        mHourPicker.setWrapSelectorWheel(true);
+        mHourPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
+        mMinutePicker.setDisplayedValues(mMinuteList.toArray(new String[mMinuteList.size()]));
+        mMinutePicker.setMaxValue(mMinuteList.size() - 1); //设置最大值
+        mMinutePicker.setWrapSelectorWheel(true);
+        mMinutePicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
+        mSecondPicker.setDisplayedValues(mSecondList.toArray(new String[mSecondList.size()]));
+        mSecondPicker.setMaxValue(mSecondList.size() - 1); //设置最大值
+        mSecondPicker.setWrapSelectorWheel(true);
+        mSecondPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //禁止输入
+
     }
 
 
